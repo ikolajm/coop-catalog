20f10b2fae64577c11fac55c8b064ec59fb09deb
ikola <ikolajm@gmail.com>
2019-02-22 10:37:33 -0500

    error handling

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const User = require('../db').import('../models/user')
+const User = require('../db').import('../models/User')
 
 const validateSession = (req, res, next) => {
   if (req.method == 'OPTIONS') {
